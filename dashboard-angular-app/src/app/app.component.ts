e7033f7130fe4500b1ac3233790755ebad59e551
Polina Tyureva <polina.tyureva@devexpress.com>
2026-02-03 13:01:19 +0400

    update to angular v21

--- dashboard-angular-app/src/app/app.component.ts
+++ dashboard-angular-app/src/app/app.component.ts
@@ -10,29 +10,23 @@ interface DashboardInfo {
 }
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    CommonModule, 
-	RouterOutlet, 
-	DxDashboardControlModule,
-	DxPopupModule,
-	DxButtonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [CommonModule, RouterOutlet, DxDashboardControlModule, DxButtonModule, DxPopupModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title : string = 'Support Traffic';
-  isPopupVisible : boolean = false;
-  dashboardId : string = 'support';
-  store : DashboardInfo[] = [
-    {"id": "support", "name": "Support Traffic"},
-    {"id": "products", "name": "Product Details"},
-  ];
+    title : string = 'Support Traffic';
+    isPopupVisible : boolean = false;
+    dashboardId : string = 'support';
+    store : DashboardInfo[] = [
+      {"id": "support", "name": "Support Traffic"},
+      {"id": "products", "name": "Product Details"},
+    ];
 
-  showDashboardInPopup(storeItem: DashboardInfo){
-    this.isPopupVisible = true; 
-    this.dashboardId = storeItem.id; 
-    this.title = storeItem.name
+    showDashboardInPopup(storeItem: DashboardInfo){
+      this.isPopupVisible = true; 
+      this.dashboardId = storeItem.id; 
+      this.title = storeItem.name
   }
 }
